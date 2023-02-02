0116db754e101a998325de70f8519ff1df71310f
congtranc0722g1 <trantancongall@gmail.com>
2023-02-02 15:46:13 +0700

    nop

---
+++ ss2_typescript/excise/fibonacci/src/index.ts
@@ -0,0 +1,14 @@
+function fibonacci(amount: number): number {
+    if (amount <= 1) return amount;
+    return fibonacci(amount - 1) + fibonacci(amount - 2);
+
+}
+
+// @ts-ignore
+let amount: number = +prompt("Nhập số lượng:");
+let sum: number = 0;
+for (let i = 0; i < amount; i++) {
+    console.log(fibonacci(i))
+    sum += fibonacci(i)
+}
+console.log("Tổng là: " + sum);
