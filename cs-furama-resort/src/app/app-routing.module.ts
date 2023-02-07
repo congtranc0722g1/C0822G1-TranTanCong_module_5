1cb9d678265dbe5e3888b9cee424660a448c219c
congtranc0722g1 <trantancongall@gmail.com>
2023-02-07 18:38:20 +0700

    nop

--- cs-furama-resort/src/app/app-routing.module.ts
+++ cs-furama-resort/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./component/home/home.component";
+import {ListFacilityComponent} from "./component/facility/list-facility/list-facility.component";
+import {CreateFacilityComponent} from "./component/facility/create-facility/create-facility.component";
+import {EditFacilityComponent} from "./component/facility/edit-facility/edit-facility.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'facility', component: ListFacilityComponent},
+  {path: 'facility-edit', component: EditFacilityComponent},
+  {path: 'facility-create', component: CreateFacilityComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cs-furama-resort/src/app/component/facility/list-facility/list-facility.component.ts
+++ cs-furama-resort/src/app/component/facility/list-facility/list-facility.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Facility} from "../../../model/facility/facility";
 
 @Component({
   selector: 'app-list-facility',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-facility.component.css']
 })
 export class ListFacilityComponent implements OnInit {
+
+  facilityList: Facility[] = [
+    {id: 1, name: "House1", facilityType: "House", area: 300, cost: 2000000,maxPeople: 10, rentType: "Năm", standardRoom: "VIP", descriptionOtherConvenience: "Không", poolArea: 20, numbersOfFloors: 3, facilityFree: "Xe đạp"},
+    {id: 2, name: "Villa1", facilityType: "Villa", area: 200, cost: 1000000,maxPeople: 5, rentType: "Tháng", standardRoom: "Common", descriptionOtherConvenience: "Không", poolArea: 25, numbersOfFloors: 5, facilityFree: "Xe máy"},
+    {id: 1, name: "Room100", facilityType: "Room", area: 250, cost: 2500000,maxPeople: 3, rentType: "Ngày", standardRoom: "VIP", descriptionOtherConvenience: "Không", poolArea: 50, numbersOfFloors: 4, facilityFree: "Ăn sáng"}
+  ];
 
   constructor() { }
 
